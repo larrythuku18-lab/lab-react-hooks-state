453daaaa3ec53ab6e9fa8b95ce98eeead4ee0ee1
Larry Thuku <larrythuku18@gmail.com>
2026-05-05 22:27:27 +0300

    Initialied the toggle Dark-mode button on the feat/DarkModeToggle

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 const DarkModeToggle = () => {
   // TODO: Implement dark mode toggle logic
+  const DarkModeToggle = ({ darkMode, setDarkMode }) => {
+  const handleToggle = () => {
+    setDarkMode((prev) => !prev)
+  }
+
+  return (
+    <button onClick={handleToggle}>
+      {darkMode ? 'Toggle Dark Mode' : 'Toggle Light Mode'}
+    </button>
+  )
+}
 
   return (
     <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
